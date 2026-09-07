53e2af59099290df8d0a4d17a7995c9a0e6309bf
Bruno Ferro <brunoferromega@outlook.com>
2026-09-07 15:40:10 -0300

    [feat/kafka] :: send with key

---
+++ kafka-began/src/main/java/io/overasync/KProducerKey.java
@@ -0,0 +1,52 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+static final Class<?> currentClass = MethodHandles.lookup().lookupClass();
+static final Logger log = LoggerFactory.getLogger(currentClass.getSimpleName());
+static final String BOOTSTRAP_SERVER = "bootstrap.servers";
+static final String KAFKA_LOCAL = "127.0.0.1:9092";
+static final String STRING_SERIALIZER = StringSerializer.class.getName();
+static final String TOPIC = "demo_jkafka";
+static final String KEY_PREFIX = "ID_";
+
+static Properties getProperties() {
+    final var properties = new Properties();
+    properties.setProperty(BOOTSTRAP_SERVER, KAFKA_LOCAL);
+    properties.setProperty("key.serializer", STRING_SERIALIZER);
+    properties.setProperty("value.serializer", STRING_SERIALIZER);
+    properties.setProperty("batch.size", "400");
+//    properties.setProperty("partition.clas", RoundRobinPartitioner.class.getName());
+    return properties;
+}
+
+void main() {
+    log.info("--Hello Kafka!--");
+    final var properties = getProperties();
+    try (final var producer = new KafkaProducer<String, String>(properties)) {
+        IntStream.range(0, 10).forEach(_ -> {
+            IntStream.range(1, 30).forEach(i -> {
+                final var key = KEY_PREFIX + i;
+                final var msg = "K Callback msg: " + i;
+                final var producerRecord = new ProducerRecord<>(TOPIC, key, msg);
+                producer.send(producerRecord, (metadata, exception) -> {
+                    if (exception != null) log.error("Something wrong had happened!", exception);
+                    log.info("""
+                            
+                            Key:       {},
+                            Topic:     {},
+                            Partition: {},
+                            """, key, metadata.topic() ,metadata.partition());
+                });
+            });
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+    log.info("--Everything done!--");
+}
